3e39f04b52f94b5381708ca00829ea51269b8798
Santiago Lamora <santiago.lamora@gmail.com>
2019-02-12 13:04:31 +0100

    Configure CI to run tests.

--- linguatec_lexicon_frontend/tests.py
+++ linguatec_lexicon_frontend/tests.py
@@ -1,18 +1,18 @@
-from django.test import TestCase
+import unittest
 
 from linguatec_lexicon_frontend.templatetags import linguatec
 
 
-class RenderEntryTestCase(TestCase):
+class RenderEntryTestCase(unittest.TestCase):
     def test_render(self):
         value = "boira (lorem ipsum)"
         html = linguatec.render_entry(value)
-        self.assertInHTML("<span class='rg-usecase-comment'>(lorem ipsum)</span>", html)
+        self.assertIn("<span class='rg-usecase-comment'>(lorem ipsum)</span>", html)
 
     def test_render_begin(self):
         value = "(foo) boira grasa"
         html = linguatec.render_entry(value)
-        self.assertInHTML("<span class='rg-usecase-comment'>(foo)</span>", html)
+        self.assertIn("<span class='rg-usecase-comment'>(foo)</span>", html)
 
     def test_render_unbalanced_parenthesis(self):
         value = "(foo)) invalid"
