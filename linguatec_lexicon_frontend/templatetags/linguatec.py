1ae488d860d85c810c70f186fa757e9413674691
Santiago Lamora <santiago.lamora@gmail.com>
2019-02-12 12:46:04 +0100

    Parse entry to apply emphasis to aragonese content
    Use parenthesis to detect annotations about usage.

--- linguatec_lexicon_frontend/templatetags/linguatec.py
+++ linguatec_lexicon_frontend/templatetags/linguatec.py
@@ -3,10 +3,27 @@ import urllib.parse
 
 from django import template
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+from linguatec_lexicon_frontend import validators
 
 
 register = template.Library()
+
+
+@register.filter
+@stringfilter
+def render_entry(value):
+    try:
+        validators.validate_balanced_parenthesis(value)
+    except ValidationError:
+        return value
+
+    value = value.replace("(", "<span class='rg-usecase-comment'>(")
+    value = value.replace(")", ")</span>")
+    return mark_safe(value)
 
 
 @register.filter
--- linguatec_lexicon_frontend/tests.py
+++ linguatec_lexicon_frontend/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from linguatec_lexicon_frontend.templatetags import linguatec
+
+
+class RenderEntryTestCase(TestCase):
+    def test_render(self):
+        value = "boira (lorem ipsum)"
+        html = linguatec.render_entry(value)
+        self.assertInHTML("<span class='rg-usecase-comment'>(lorem ipsum)</span>", html)
+
+    def test_render_begin(self):
+        value = "(foo) boira grasa"
+        html = linguatec.render_entry(value)
+        self.assertInHTML("<span class='rg-usecase-comment'>(foo)</span>", html)
+
+    def test_render_unbalanced_parenthesis(self):
+        value = "(foo)) invalid"
+        html = linguatec.render_entry(value)
+        self.assertEqual(value, html)
