ecadf0c886a8e025f867340be0c038fb18aefabe
Santiago Lamora <santiago.lamora@gmail.com>
2019-02-13 14:04:25 +0100

    Fixes #20 use mock to avoid calling external API

--- linguatec_lexicon_frontend/tests.py
+++ linguatec_lexicon_frontend/tests.py
@@ -1,15 +1,21 @@
 import unittest
 
+from unittest import mock
+
 from linguatec_lexicon_frontend.templatetags import linguatec
 
 
 class RenderEntryTestCase(unittest.TestCase):
-    def test_render(self):
+    @unittest.mock.patch('linguatec_lexicon_frontend.utils.retrieve_gramcats')
+    def test_render(self, retrieve_gramcats):
+        retrieve_gramcats.return_value = []
         value = "boira (lorem ipsum)"
         html = linguatec.render_entry(value)
         self.assertIn("<span class='rg-usecase-comment'>(lorem ipsum)</span>", html)
 
-    def test_render_begin(self):
+    @unittest.mock.patch('linguatec_lexicon_frontend.utils.retrieve_gramcats')
+    def test_render_begin(self, retrieve_gramcats):
+        retrieve_gramcats.return_value = []
         value = "(foo) boira grasa"
         html = linguatec.render_entry(value)
         self.assertIn("<span class='rg-usecase-comment'>(foo)</span>", html)
