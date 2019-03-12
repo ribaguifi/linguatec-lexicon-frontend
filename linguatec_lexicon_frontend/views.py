39441d67214425491170c1a338a3157d52e40e52
Santiago Lamora <santiago.lamora@gmail.com>
2019-03-12 12:50:19 +0100

    [qa] remove unused import.

--- linguatec_lexicon_frontend/views.py
+++ linguatec_lexicon_frontend/views.py
@@ -4,7 +4,6 @@ The views.
 import urllib.parse
 
 from django.conf import settings
-from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 from django.urls import resolve
